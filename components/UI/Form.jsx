502272ae90950dafcc5f3220373124c32791a11d
Shubham Patel <shubhamprecious1@gmail.com>
2022-08-04 09:35:42 +0530

    Update Form.jsx

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import classes from '../../styles/form.module.css'
 
 const Form = () => {
-  return <form className={`${classes.form}`}>
+
+   async function handleOnSubmit(e) {
+      e.preventDefault();
+      const formData = {}
+      Array.from(e.currentTarget.elements).forEach(field =>{
+         if( !field.name ) return;
+         formData[field.name] = field.value;
+      });
+
+      console.log(formData)
+   }
+
+
+  return <form className={`${classes.form}`} method='post' onSubmit={handleOnSubmit}>
    <div className={`${classes.form__group}`}>
       <input type='text' placeholder='Your Name' required />
    </div>
