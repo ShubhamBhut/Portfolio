a505f9973412755f928c2b617529a798926585fc
Shubham Patel <shubhamprecious1@gmail.com>
2022-10-29 09:06:20 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,12 @@ import 'remixicon/fonts/remixicon.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from '../components/Layout/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
     <Component {...pageProps} />
+    <Analytics />
   </Layout>
 }
 
